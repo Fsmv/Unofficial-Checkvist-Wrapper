fbd5c4608238ec547e23a085194e465089563474
Andrew Kallmeyer <fsmv@sapium.net>
2016-05-03 23:14:02 -0400

    Basic WebView wrapper

---
+++ app/src/main/java/net/sapium/checkvistwrapper/Checkvist.java
@@ -0,0 +1,124 @@
+package net.sapium.checkvistwrapper;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.webkit.ClientCertRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.app.Activity;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+import java.io.File;
+
+public class Checkvist extends AppCompatActivity {
+
+    private WebView mWebView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkvist);
+        addProgressBar();
+
+        mWebView = (WebView) findViewById(R.id.activity_checkvist_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+
+        webSettings.setAppCachePath(this.getCacheDir().getAbsolutePath());
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setAppCacheEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (Uri.parse(url).getHost().endsWith("checkvist.com")) {
+                    return false;
+                }
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                view.getContext().startActivity(intent);
+                return true;
+            }
+        });
+
+        setProgressBarVisibility(true);
+        final Checkvist checkvist = this;
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                checkvist.setProgressAmount(progress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                setTitle(title);
+            }
+        });
+
+        mWebView.loadUrl("http://m.checkvist.com");
+    }
+
+    protected void setProgressAmount(int amount) {
+        progressBar.setProgress(amount);
+        if (amount == 100) {
+            progressBar.setVisibility(View.INVISIBLE);
+        } else {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    // http://stackoverflow.com/questions/13934010/progressbar-under-action-bar
+    private void addProgressBar() {
+        // create new ProgressBar and style it
+        progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal);
+        progressBar.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, 48));
+
+        // retrieve the top view of our application
+        final FrameLayout decorView = (FrameLayout) getWindow().getDecorView();
+        decorView.addView(progressBar);
+
+        // Here we try to position the ProgressBar to the correct position by looking
+        // at the position where content area starts. But during creating time, sizes
+        // of the components are not set yet, so we have to wait until the components
+        // has been laid out
+        // Also note that doing progressBar.setY(136) will not work, because of different
+        // screen densities and different sizes of actionBar
+        ViewTreeObserver observer = progressBar.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                View contentView = decorView.findViewById(android.R.id.content);
+                progressBar.setY(contentView.getY() + 55);
+
+                ViewTreeObserver observer = progressBar.getViewTreeObserver();
+                observer.removeOnGlobalLayoutListener(this);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
